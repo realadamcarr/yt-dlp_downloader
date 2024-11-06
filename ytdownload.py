b55ddb77b4adafff83aad8dd002c53cd1ac8ab53
Adam Carr <156648324+realadamcarr@users.noreply.github.com>
2024-11-06 20:44:11 +0000

    refactored code

--- ytdownload.py
+++ ytdownload.py
@@ -1,26 +1,5 @@
 from yt_dlp import YoutubeDL
 
-URLS = ['https://www.youtube.com/watch?v=BaW_jenozKc']
-
-
-# x = input()
-# with YoutubeDL() as ydl:
-#     ydl.download(x)
-
-def download_video(url):
-    try:
-        # Create a YouTube object using the provided URL
-
-        # Display the video title
-        print(f"Downloading {yt.title}")
-
-        # Get the highest resolution stream and download it
-        yt.streams.get_highest_resolution().download()
-        print('Download completed!')
-
-    except Exception as e:
-        # Print any error that occurs during the download process
-        print(f"An error occurred: {e}")
 
 
 # Prompt the user to enter the YouTube video URL
